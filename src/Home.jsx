e8cf6fd2fe2c65c34968eb55b52c8c4c0513b8ed
Mee-Lensey <148074806+Mee-Lensey@users.noreply.github.com>
2026-01-07 21:14:01 -0500

    React JSON

---
+++ src/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Colors from './Colors';
+
+const Home=()=>{
+    return(
+        <>
+        <h1>This is home component</h1>
+        <Colors/>
+
+        </>
+    )
+}
+
+export default Home;
