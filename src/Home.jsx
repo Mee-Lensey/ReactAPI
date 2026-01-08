706a514383ee3b0054d91ccef67a5672140481fd
Mee-Lensey <148074806+Mee-Lensey@users.noreply.github.com>
2026-01-08 10:59:48 -0500

    Action Movies

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,10 +1,37 @@
 import React from 'react';
 import Colors from './Colors';
+import axios from 'axios';
+
+
+const [Data,setData]=UseState({
+    Company:'',
+    Description:'',
+})
+const [colorsData,setColorsData]=useState([])
 
 const Home=()=>{
+
+
+    useEffect(()=>{
+     axios.get('https://reqres.in/api/unknown',  { headers: { 'x-api-key': 'reqres_bc833d78e78f49b297257c99fa851391'}})
+        .then(res=>{
+            //make the name and description your own fictional company
+            let companyName = "Action Movies";
+            let CompanyDesc= 
+            console.log('Response from Main API:' ,res);
+            console.log('Colors Data:', res.data.data);
+
+        })
+        .catch(err=>{
+
+            console.log(err);
+
+        })
+     }, [])
+    
     return(
         <>
-        <h1>This is home component</h1>
+       
         <Colors/>
 
         </>
