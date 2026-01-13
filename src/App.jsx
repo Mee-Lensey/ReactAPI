b0a5d9a6ae385367f3bbc911db3a2edcb571f484
Mee-Lensey <148074806+Mee-Lensey@users.noreply.github.com>
2026-01-13 11:11:02 -0500

    data in the colors jsx

--- src/App.jsx
+++ src/App.jsx
@@ -8,33 +8,13 @@ function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <div className="App">
-      <div>
-        <a href="https://vite.dev" target="_blank" rel="noreferrer noopener">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank" rel="noreferrer noopener">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
+   
 
-      <h2>React JSON</h2>
+
       <Home />
 
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((c) => c + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
+    
 
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
   )
 }
 
--- src/Home.jsx
+++ src/Home.jsx
@@ -1,35 +1,36 @@
-import React from 'react';
+import React,{useEffect, useState} from 'react';
 import Colors from './Colors';
 import axios from 'axios';
 
+const Home=()=>{
 
-const [Data,setData]=UseState({
+const [Data,setData]=useState({ //comeback to this
     Company:'',
     Description:'',
 })
-const [colorsData,setColorsData]=useState([])
-
-const Home=()=>{
-
+const [colorsData,setColorsData]=useState([]) //come back to this
 
     useEffect(()=>{
      axios.get('https://reqres.in/api/unknown',  { headers: { 'x-api-key': 'reqres_bc833d78e78f49b297257c99fa851391'}})
         .then(res=>{
+
             //make the name and description your own fictional company
             let companyName = "Action Movies";
             let CompanyDesc= " We provide action packed movies for kids."
-            console.log('Response from Main API:' ,res);
-            console.log('Colors Data:', res.data.data);
+
+            setData({Company:companyName, Description:CompanyDesc})
+            setColorsData(res.data.data)
+          
 
         })
         .catch(err=>{
-
-            console.log(err);
-
-        })
+        console.log(err);
+         })
      }, [])
     
     return(
+
+         
         <>
         <h1>{Data.Company}</h1>
         <p>{Data.Description}</p>
